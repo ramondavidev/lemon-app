9643d767b469ff955572690dff2ec26e52457acb
Ramon <ramondavi@msn.com>
2025-07-29 21:37:35 -0300

    set up MF with Zephyr

--- app2/src/App.jsx
+++ app2/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./assets/lemon.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<div className="App">
-			<div>
-				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Rspack + React</h1>
-			<div className="card">
-				<button type="button" onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Rspack and React logos to learn more
-			</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Rspack + React</h1>
+      <div className="card">
+        <button
+          type="button"
+          className="lemon-button"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          <img src={reactLogo} alt="Lemon" className="lemon-icon" />
+          count is {count}
+        </button>
+        <h1 className="title">
+          Deployed application with
+          <span className="zephyr">Zephyr</span>
+          and
+          <span className="mf">Module Federation</span>
+        </h1>
+      </div>
+      <p className="footer">Created by Ramon 🍋</p>
+    </div>
+  );
 }
 
 export default App;
